b54695d49fd4372bd5ed86c9ab675396e0171ba4
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-12-05 16:15:47 +0700

    membuat stack menu

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -57,11 +57,26 @@ namespace Exercise4
                 }
             }
         }
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
 
 
 
+            }
+        }
     }
 }
+
 
         
     
