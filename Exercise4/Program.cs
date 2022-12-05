ece101924e1422f37e250b22db2248543f634e7c
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-12-05 16:00:21 +0700

    Add project files.

---
+++ Exercise4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+struct STACK  //Membuat jenis data abstrak 'STACK'
+{
+    int TOP;
+    float data[88];
+
+};
+float dta;
+struct STACK stackbaru;
+
+        
+    
+
